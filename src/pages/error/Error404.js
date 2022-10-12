8db5b92475c8a722939409ec75e26bc3b462dbc0
saown <songsaown@gmail.com>
2022-10-12 22:03:25 +0600

    bugs

--- src/pages/error/Error404.js
+++ src/pages/error/Error404.js
@@ -1,13 +1,20 @@
 import React, {useEffect} from "react";
 const Error404 = (props) => {
+
     const {title} = props;
+
     useEffect(()=>{
+
         document.title = title;
+
     },[title])
+
     return(
+
         <div className="vh-100 d-flex justify-content-center align-items-center">
             <h1>This Page Is Not Found</h1>
         </div>
+
     )
 }
 export default Error404;
